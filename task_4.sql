01c7bdfaaec6e4139b6d809359bfc605252b4b35
Joslin ISHIMWE <joslinishimwe@Mac.home>
2024-11-13 20:19:56 -0600

    filename 5

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,1 @@
--- SQL script to print a basic description of the 'books' table
-SELECT COLUMN_NAME, DATA_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'  -- Specify the database name directly
-  AND TABLE_NAME = 'Books';
+SELECT * FROM Books LIMIT 0;
