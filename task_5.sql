42f7350f3f53ba6be6592c609cb688835854dd8d
Joslin ISHIMWE <joslinishimwe@Mac.home>
2024-11-13 20:33:41 -0600

    upload new file2

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,4 @@
 -- SQL script to insert a single row into the 'customer' table in the 'alx_book_store' database
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
