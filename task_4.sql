bea56007aea4661e8c66885ee3c6db5c232a5513
Joslin ISHIMWE <joslinishimwe@Mac.home>
2024-11-13 20:20:54 -0600

    filename 6

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,7 @@
-SELECT * FROM Books LIMIT 0;
+-- SQL script to print the full description of the 'Books' table from the 'alx_book_store' database
+
+SELECT COLUMN_NAME, 
+       COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'  -- Specify the database name explicitly
+  AND TABLE_NAME = 'Books';
