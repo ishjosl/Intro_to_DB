ba0783874de34bd516ab594bab282291d6533bc7
Joslin ISHIMWE <joslinishimwe@Mac.home>
2024-11-13 20:14:45 -0600

    filename 2

--- task_4.sql
+++ task_4.sql
@@ -1,10 +1,12 @@
+-- SQL script to print the full description of the 'books' table from the provided database
+
 SELECT COLUMN_NAME, 
        DATA_TYPE, 
        IS_NULLABLE, 
        COLUMN_DEFAULT, 
        CHARACTER_MAXIMUM_LENGTH, 
        NUMERIC_PRECISION, 
-       NUMERIC_SCALE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()  -- Use the database passed as an argument
+       NUMERIC_SCALE 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = DATABASE()  -- The database name is passed as an argument to the mysql command
   AND TABLE_NAME = 'books';
